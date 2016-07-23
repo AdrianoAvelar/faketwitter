de68664780d89fba96fa515ac4582ea94ca8953d
Adriano Avelar <eam.avelar@gmail.com>
2016-07-23 15:38:15 +0000

    First Controller Pages that controls the index, explore, profile and home pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+#Contém todo o código de qualquer página dentro de /views/pages
+class PagesController < ApplicationController
+  
+  #Código back-end para /view/pages/index
+  def index
+  end
+
+ #Código back-end para /view/pages/home
+  def home
+  end
+
+ #Código back-end para /view/pages/profile
+  def profile
+  end
+
+ #Código back-end para /view/pages/explore
+  def explore
+  end
+end
